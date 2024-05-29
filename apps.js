50ec1b20c4404881bbcb1ad3619b0f3048e3f57c
SHIFA NAWED <156599970+SHIFANAWED1234@users.noreply.github.com>
2024-05-30 00:26:26 +0500

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,75 @@
+/////////ASSIGNEMENT NO 1 //////////
+//  Write a script to greet your website visitor using JS alert box 
+alert ("HELLO WORLD!")
+
+// Write a script to display following message on your web page  
+alert ('error! please enter a valid passward' )
+
+// Write a script to display following message on your web page 
+alert ('Welcome To JS Land... \nHappy Coding! ')
+
+// Write a script to display following messages in sequence:
+alert ("welcome to JS land... ")
+// 
+alert ("happy coding!")
+
+// Generate the following message through browser’s developer console:
+alert ("Hello.... I can runs JS through my web browser's console")
+
+/////////ASSIGNEMENT NO 2///////
+// Declare a variable called username.
+let name = "SHIFA"; //assigned string value
+///Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+alert ('SHIFA')
+//
+alert ('my age is 16') 
+//
+alert ('web and moble application development ')
+//Write a script to display the following alert using one JS variable:
+alert ('PIZZA \nPIZZ \nPIZ \nPI \nP')
+
+//Declare a variable called email and assign to it a string that represents your Email Address.
+alert ('my email address is shifanawed3@gmail.com')
+
+//Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following   message in an alert box:
+alert ('i am trying to learn from the book \nA Smarter Way To Learn Javascript')
+
+///////ASSIGNMENT NO 3 ////////
+//Declare a variable called age & assign to it your age. Show your age in an alert box.
+alert ('i am 16 year old ')
+
+//Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”.
+alert ('you have visited this sites 14 times ')
+
+//////ASSIGENMENT NO 5 ///////////
+///Write a program that take two numbers & add them in a new variable. Show the result in your browser.
+let num = 3;
+alert  (++num);
+alert(num);//4
+
+///Repeat task1 for subtraction, multiplication, division & modulus.
+let num1 = 3;
+alert (--num1);
+alert (num1);//2
+///
+/////Cost of one movie ticket is 600 PKR. Write a script to store ticket price in a variable & calculate the cost of buying 5 tickets to a movie.
+let num0 =600
+alert('total cost of 5 ticket to a movies is 3000PKR')
+///Write a script to display multiplication table of any number in your browser.
+
+/* JavaScript program to generate a multiplication table of a particular range */
+
+var num8 = parseInt(prompt('Enter an integer: '));
+var range = parseInt(prompt('Enter a range: '));
+
+//creating a multiplication table
+for(let i = 1; i <= range; i++) {
+    var res = i * num;
+    console.log(num +" * "+ i + " = " + res);
+}
+
+
+
+
+
+
